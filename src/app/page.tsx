078b0d7bbee4921aef9db96d1d82b7041f6b404e
jackvdp <jack@pumpymusic.co.uk>
2025-12-27 14:39:20 +0000

    Add navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import { Hero } from "@/components/hero";
+import { Navbar } from "@/components/navbar";
 
 export default function Home() {
   return (
     <main className="dark">
+      <Navbar />
       <Hero />
     </main>
   );
